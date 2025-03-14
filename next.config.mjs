7fa7ecb1393fb6e8d0e0bec9484f4f48ac758e55
Vetrans <114501970+Vetrans@users.noreply.github.com>
2025-03-14 21:33:14 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,31 @@
-import {withSentryConfig} from "@sentry/nextjs";
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+import {withSentryConfig} from '@sentry/nextjs';
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 };
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
-// https://www.npmjs.com/package/@sentry/webpack-plugin#options
+// https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "hrishabh",
+// Suppresses source map uploading logs during build
+silent: true,
+org: "jsm-x9",
 project: "javascript-nextjs",
-
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
-
+}, {
 // For all available options, see:
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
 // Upload a larger set of source maps for prettier stack traces (increases build time)
 widenClientFileUpload: true,
 
-// Automatically annotate React components to show their full name in breadcrumbs and session replay
-reactComponentAnnotation: {
-enabled: true,
-},
+// Transpiles SDK to be compatible with IE11 (increases bundle size)
+transpileClientSDK: true,
 
 // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
 // This can increase your server load as well as your hosting bill.
@@ -32,10 +33,13 @@ enabled: true,
 // side errors will fail.
 // tunnelRoute: "/monitoring",
 
+// Hides source maps from generated client bundles
+hideSourceMaps: true,
+
 // Automatically tree-shake Sentry logger statements to reduce bundle size
 disableLogger: true,
 
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+// Enables automatic instrumentation of Vercel Cron Monitors.
 // See the following for more information:
 // https://docs.sentry.io/product/crons/
 // https://vercel.com/docs/cron-jobs
